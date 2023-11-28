fd7a5835c720ea37bb1524f173a33ce21854c602
Pedro Henrique Alves dos Santos <pedro.santos@trinusco.com.br>
2023-11-28 04:51:50 -0300

    Improving views_game logic

--- views_game.py
+++ views_game.py
@@ -4,84 +4,98 @@ from models import Games
 from helpers import recover_image, delete_file, GameForm
 import time
 
+def is_user_logged_in():
+    return 'logged_user' in session and session['logged_user'] is not None
+
+def get_game(id):
+    return Games.query.filter_by(id=id).first()
+
 @app.route('/')
 def index():
-  game_list = Games.query.order_by(Games.id)
-  return render_template('list.html', title='Games', games=game_list)
+    game_list = Games.query.order_by(Games.id)
+    return render_template('list.html', title='Games', games=game_list)
 
 @app.route('/new')
 def new():
-  if 'logged_user' not in session or session['logged_user'] == None:
-    return redirect(url_for('signin', next_page=url_for('new')))
-  form = GameForm()
-  return render_template('new.html', title='New Game', form=form)
+    if not is_user_logged_in():
+        return redirect(url_for('signin', next_page=url_for('new')))
+    
+    form = GameForm()
+    return render_template('new.html', title='New Game', form=form)
 
 @app.route('/create', methods=['POST',])
 def create():
-  form = GameForm(request.form)
-  if not form.validate_on_submit():
-    return redirect(url_for('new'))
-  name = form.name.data
-  category = form.category.data
-  platform = form.platform.data
-  game = Games.query.filter_by(name=name).first()
-  if game:
-    flash('This game already exists')
-    return redirect(url_for('index'))
-  new_game = Games(name=name, category=category, platform=platform)
-  db.session.add(new_game)
-  db.session.commit()
-  file = request.files['file']
-  upload_path = app.config['UPLOAD_PATH']
-  timestamp = time.time()
-  file_name = f'cover_{new_game.id}-{timestamp}.jpg'
-  save_path = f'{upload_path}/{file_name}'
-  file.save(save_path)
-
-  return redirect(url_for('index'))
-
-@app.route('/edit/<int:id>')
-def edit(id):
-  if 'logged_user' not in session or session['logged_user'] == None:
-    return redirect(url_for('signin', next_page=url_for('edit')))
-  game = Games.query.filter_by(id=id).first()
-  form = GameForm()
-  form.name.data = game.name
-  form.category.data = game.category
-  form.platform.data = game.platform
-  game_cover = recover_image(id)
-  return render_template('edit.html', title='Editing Game', id=id, game_cover=game_cover, form=form)
-
-@app.route('/update', methods=['POST',])
-def update():
-  form = GameForm(request.form)
-
-  if form.validate_on_submit():
-    game = Games.query.filter_by(id=request.form['id']).first()
-    game.name = form.name.data
-    game.category = form.category.data
-    game.platform = form.platform.data
-    db.session.add(game)
+    form = GameForm(request.form)
+    
+    if not form.validate_on_submit():
+        return redirect(url_for('new'))
+    
+    name = form.name.data
+    category = form.category.data
+    platform = form.platform.data
+    game = Games.query.filter_by(name=name).first()
+    
+    if game:
+        flash('This game already exists')
+        return redirect(url_for('index'))
+    
+    new_game = Games(name=name, category=category, platform=platform)
+    db.session.add(new_game)
     db.session.commit()
+    
     file = request.files['file']
     upload_path = app.config['UPLOAD_PATH']
     timestamp = time.time()
-    delete_file(game.id)
-    file_name = f'cover_{game.id}-{timestamp}.jpg'
+    file_name = f'cover_{new_game.id}-{timestamp}.jpg'
     save_path = f'{upload_path}/{file_name}'
     file.save(save_path)
+    
+    return redirect(url_for('index'))
 
-  return redirect(url_for('index'))
+@app.route('/edit/<int:id>')
+def edit(id):
+    if not is_user_logged_in():
+        return redirect(url_for('signin', next_page=url_for('edit')))
+    
+    game = get_game(id)
+    form = GameForm(obj=game)
+    game_cover = recover_image(id)
+    
+    return render_template('edit.html', title='Editing Game', id=id, game_cover=game_cover, form=form)
+
+@app.route('/update', methods=['POST',])
+def update():
+    form = GameForm(request.form)
+    
+    if form.validate_on_submit():
+        game = get_game(request.form['id'])
+        game.name = form.name.data
+        game.category = form.category.data
+        game.platform = form.platform.data
+        db.session.add(game)
+        db.session.commit()
+        
+        file = request.files['file']
+        upload_path = app.config['UPLOAD_PATH']
+        timestamp = time.time()
+        delete_file(game.id)
+        file_name = f'cover_{game.id}-{timestamp}.jpg'
+        save_path = f'{upload_path}/{file_name}'
+        file.save(save_path)
+    
+    return redirect(url_for('index'))
 
 @app.route('/delete/<int:id>')
 def delete(id):
-  if 'logged_user' not in session or session['logged_user'] == None:
-    return redirect(url_for('signin'))
-  Games.query.filter_by(id=id).delete()
-  db.session.commit()
-  flash('Game deleted successfully!')
-  return redirect(url_for('index'))
+    if not is_user_logged_in():
+        return redirect(url_for('signin'))
+    
+    Games.query.filter_by(id=id).delete()
+    db.session.commit()
+    
+    flash('Game deleted successfully!')
+    return redirect(url_for('index'))
 
 @app.route('/uploads/<file_name>')
 def image(file_name):
-  return send_from_directory('uploads', file_name)
+    return send_from_directory('uploads', file_name)
